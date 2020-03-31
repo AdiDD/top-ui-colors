625e2b97bf619f053bea08906b77b7ba90e30a4d
AdiDD <adi.deaconu21@gmail.com>
2020-04-01 02:40:20 +0300

    Deleted all CSS files (exceptind App.css) and created JSS file for each

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,40 +1,7 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/styles";
 import MiniPalette from "./MiniPalette";
-
-const styles = {
-  root: {
-    background: "blue",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start"
-  },
-  container: {
-    width: "50%",
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap"
-  },
-  nav: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    color: "white",
-    "& h1": {
-      margin: "35px 0",
-      fontWeight: "700"
-    }
-  },
-  palettes: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
-  }
-};
+import styles from "./styles/PaletteListStyles";
 
 class PaletteList extends Component {
   goToPalette(id) {
---
+++ src/styles/PaletteListStyles.js
@@ -0,0 +1,33 @@
+export default {
+  root: {
+    background: "blue",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white",
+    "& h1": {
+      margin: "35px 0",
+      fontWeight: "700"
+    }
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+};
